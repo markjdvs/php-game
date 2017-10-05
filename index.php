fae9ac60f0f3ce0b1f56d520cd85d73bfc43d6b5
mrkjdvs <mjd814@gmail.com>
2017-10-05 17:32:33 +0100

    Calculate winner function created. Running once after 1P choice, again after CPU choice.

--- index.php
+++ index.php
@@ -13,14 +13,52 @@
         for ($i = 0; $i < 9; $i++) {
           $board[$i] = $_POST[$i];
         }
+
+        $lines = array(
+          array(0,1,2),
+          array(3,4,5),
+          array(6,7,8),
+          array(0,3,6),
+          array(1,4,7),
+          array(2,5,8),
+          array(0,4,8),
+          array(2,4,6)
+        );
+
+        function calculateWinner() {
+          for($i = 0; $i < 9; $i++) {
+            $testArray = $lines[$i];
+            if($board[$testArray[0]] && $board[$testArray[0]] == $board[$testArray[1]] && $board[$testArray[0]] == $board[$testArray[2]]) {
+              print $board[$testArray[0]] . "'s win!";
+            }
+          }
+        }
+
+        calculateWinner();
+
+        $empty = 0;
+        for ($i = 0; $i < 9; $i++) {
+          if ($board[$i] == '') $empty = 1;
+        }
+
+        if ($empty == 1) {
+          $i = rand(0,8);
+          while($board[$i] != '') {
+            $i = rand(0,8);
+          }
+          $board[$i] = 'o';
+        }
+
+        calculateWinner();
+
       }
     ?>
     <form method="POST" action="index.php">
         <?php
-        for($i = 0; $i < 9; $i++) {
-          print "<input name=$i type=text value=$board[$i]>";
-        }
-        print "<input type=submit name=submit value=submit>";
+          for($i = 0; $i < 9; $i++) {
+            print "<input name=$i type=text value=$board[$i]>";
+          }
+          print "<input type=submit name=submit value=submit>";
         ?>
     </form>
 
