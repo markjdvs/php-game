953c7e778ff6c4c0dbfea91ac59cd38da0163cbf
mrkjdvs <mjd814@gmail.com>
2017-10-05 13:43:03 +0100

    Initial commit and basic dice set up.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $dice = array(
+              1 => 1/6,
+              2 => 1/6,
+              3 => 1/6,
+              4 => 1/6,
+              5 => 1/6,
+              6 => 1/6);
+      echo $dice[1];
+    ?>
+  </body>
+</html>
