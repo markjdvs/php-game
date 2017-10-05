fbdd8f7238e846c662976afcf054ff486c77fe13
mrkjdvs <mjd814@gmail.com>
2017-10-05 16:27:50 +0100

    Value persisting after submission of turn.

--- index.php
+++ index.php
@@ -10,13 +10,15 @@
       $board = array();
       for ($i = 0; $i < 9; $i++) array_push($board, '');
       if(isset($_POST["submit"])) {
-
+        for ($i = 0; $i < 9; $i++) {
+          $board[$i] = $_POST[$i];
+        }
       }
     ?>
     <form method="POST" action="index.php">
         <?php
         for($i = 0; $i < 9; $i++) {
-          print "<input name=$i type=text value>";
+          print "<input name=$i type=text value=$board[$i]>";
         }
         print "<input type=submit name=submit value=submit>";
         ?>
